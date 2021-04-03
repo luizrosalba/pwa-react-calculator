8383ffa88badd783c76bee59305f74e6fc7fa4a1
luizrosalba <luizrosalba@gmail.com>
2021-04-03 17:03:59 -0300

    showing button inner in input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import React, {useState} from 'react';
+
 import './App.css';
 
 import Botoes from './components/Botoes'
 
-function onClick (event){
-  console.log(event.target.innerText)
-}
 
 function App() {
+  const [entrada, setValue] = useState(0);
+
+  const handleEntrada = (event) => {
+
+    const newEntrada = event.target.innerText;
+    setValue(newEntrada);
+
+  }
+
   return (
 
     <div className="App">
       <header className="App-header">
-          <h1> Projeto Calculadora </h1> 
+          <h1>  Projeto Calculadora Simples </h1> 
       </header>
       <section>
         <div className="container">
           <div className="wrap">
             <div>
-              <input className="entrada" />
+              <input 
+                handleAddValue={handleEntrada} 
+                value={entrada}
+                className="entrada" 
+              />
             </div>
             <Botoes 
-              onClick= {onClick}
+              onClick= {handleEntrada}
             />
           </div>
         </div>
